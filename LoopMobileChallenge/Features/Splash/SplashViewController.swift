fb0b21cfbd2663f988ea40cf2d91e61559cae8b8
iMarko00 <markomisic75@gmail.com>
2026-08-03 22:50:42 +0200

    impl. appIcon; impl. SplashScreen

---
+++ LoopMobileChallenge/Features/Splash/SplashViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+
+final class SplashViewController: UIViewController {
+    private let spinner = UIActivityIndicatorView(style: .large)
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Fetching the newest movies..."
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        label.font = UIFont(name: "SFProText-Semibold", size: 18) ?? .systemFont(ofSize: 18, weight: .semibold)
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        setupUI()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        spinner.startAnimating()
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.2) { [weak self] in
+            self?.showSignUp()
+        }
+    }
+
+    private func setupUI() {
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(spinner)
+        view.addSubview(subtitleLabel)
+
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -14),
+
+            subtitleLabel.topAnchor.constraint(equalTo: spinner.bottomAnchor, constant: 18),
+            subtitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            subtitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+    }
+
+    private func showSignUp() {
+        guard presentedViewController == nil else { return }
+
+        let signUpViewController = SignUpViewController()
+        signUpViewController.modalPresentationStyle = .fullScreen
+        signUpViewController.modalTransitionStyle = .crossDissolve
+        present(signUpViewController, animated: true)
+    }
+}
